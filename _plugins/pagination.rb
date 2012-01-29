69e8989c86a3f22eabbc64634b96c3876b7a01a2
Dmitry Nix <dmitry.nix@gmail.com>
2012-01-29 17:09:33 -0300

    Replaced pagination plugin with jekyll paginator

--- _plugins/pagination.rb
+++ _plugins/pagination.rb
@@ -11,7 +11,7 @@ module Jekyll
     # Returns nothing.
     def generate(site)
       site.pages.dup.each do |page|
-        paginate(site, page) if Pager.pagination_enabled?(site.config, page)
+        paginate(site, page) if Pager.pagination_enabled?(site.config, page.name)
       end
     end
 
@@ -32,16 +32,12 @@ module Jekyll
     def paginate(site, page)
       all_posts = site.site_payload['site']['posts']
       pages = Pager.calculate_pages(all_posts, site.config['paginate'].to_i)
-      page_dir = page.destination('').sub(/\/[^\/]+$/, '')
-      page_dir_config = site.config['pagination_dir']
-      dir = ((page_dir_config || page_dir) + '/').sub(/^\/+/, '')
-
       (1..pages).each do |num_page|
-        pager = Pager.new(site.config, num_page, all_posts, page_dir+'/', '/'+dir, pages)
+        pager = Pager.new(site.config, num_page, all_posts, pages)
         if num_page > 1
-          newpage = Page.new(site, site.source, page_dir, page.name)
+          newpage = Page.new(site, site.source, page.dir, page.name)
           newpage.pager = pager
-          newpage.dir = File.join(page.dir, "#{dir}page/#{num_page}")
+          newpage.dir = File.join(page.dir, "page#{num_page}")
           site.pages << newpage
         else
           page.pager = pager
@@ -70,7 +66,7 @@ module Jekyll
     #
     # Returns true if pagination is enabled, false otherwise.
     def self.pagination_enabled?(config, file)
-      file.name == 'index.html' && !config['paginate'].nil? && file.content =~ /paginator\./
+      file == 'index.html' && !config['paginate'].nil?
     end
 
     # Initialize a new Pager.
@@ -80,12 +76,10 @@ module Jekyll
     # all_posts - The Array of all the site's Posts.
     # num_pages - The Integer number of pages or nil if you'd like the number
     #             of pages calculated.
-    def initialize(config, page, all_posts, index_dir, pagination_dir, num_pages = nil)
+    def initialize(config, page, all_posts, num_pages = nil)
       @page = page
       @per_page = config['paginate'].to_i
-      @page_dir = pagination_dir + 'page/'
       @total_pages = num_pages || Pager.calculate_pages(all_posts, @per_page)
-      @previous_page = nil
 
       if @page > @total_pages
         raise RuntimeError, "page number can't be greater than total pages: #{@page} > #{@total_pages}"
@@ -96,9 +90,8 @@ module Jekyll
 
       @total_posts = all_posts.size
       @posts = all_posts[init..offset]
-      @previous_page = @page != 1 ? @page_dir + (@page - 1).to_s + '/' : nil
-      @previous_page = index_dir if @page - 1 == 1
-      @next_page = @page != @total_pages ? @page_dir + (@page + 1).to_s + '/' : nil
+      @previous_page = @page != 1 ? @page - 1 : nil
+      @next_page = @page != @total_pages ? @page + 1 : nil
     end
 
     # Convert this Pager's data to a Hash suitable for use by Liquid.
@@ -116,6 +109,4 @@ module Jekyll
       }
     end
   end
-
 end
-
